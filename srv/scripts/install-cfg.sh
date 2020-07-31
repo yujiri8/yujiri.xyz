5adc689a7f18d63c7982c4618ba864e256e0cd4f
yujiri8 <rlwestlund@gmail.com>
2020-07-31 20:21:36 +0000

    fix /usr/local/etc/nginx/sites symlink being nested

--- srv/scripts/install-cfg.sh
+++ srv/scripts/install-cfg.sh
@@ -5,6 +5,6 @@ dir=~/src/srv/cfg
 ln -f $dir/paladin.conf /usr/local/etc/
 ln -f $dir/nginx/nginx.conf /usr/local/etc/nginx/
 ln -f $dir/nginx/redirect-map.conf /usr/local/etc/nginx/
-ln -sf $dir/nginx/sites /usr/local/etc/nginx/sites
+ln -sf $dir/nginx/sites /usr/local/etc/nginx/
 ln -f $dir/torrc /usr/local/etc/tor/
 ln -f $dir/yggdrasil.conf /usr/local/etc/
